ce63390155871fbba5043e0a9006b817d2b964ef
Bobby Aleem <78314667+baxsm@users.noreply.github.com>
2022-10-03 20:31:19 +0500

    update

--- script.js
+++ script.js
@@ -26,3 +26,35 @@ var swiperProducts = new Swiper(".products__container", {
         }
     }
 });
+
+const sections = document.querySelectorAll('section[id]');
+
+const scrollActive = () => {
+    const scrollY = window.scrollY;
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 58;
+        const sectionId = current.getAttribute('id');
+        const sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add('active-link');
+        }
+        else {
+            sectionsClass.classList.remove('active-link');
+        }
+    });
+}
+
+window.addEventListener('scroll', scrollActive);
+
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up');
+    if(this.scrollY >= 350) {
+        scrollUp.classList.add('show-scroll');
+    }
+    else {
+        scrollUp.classList.remove('show-scroll')
+    }
+}
+
+window.addEventListener('scroll', scrollUp)
